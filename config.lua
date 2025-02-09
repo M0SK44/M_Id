71cdf27d83f62fc203a6a92c34bc57999d827ca7
Bastian Contreras <bastiancontrerasvillalobos@gmail.com>
2025-02-09 12:16:29 -0300

    Actualización del código con nuevos cambios

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
-Config.DiscordWebHook = "https://discord.com/api/webhooks/1048124868627017778/m4WACxK35kzYTScnGu9jdg8pjo0-kLPsuOUvX-ezp3WL22wYesu8P567yt7GL23SQGb4"
-Config.ShowIdCommand = "showid"
+Config.DiscordWebHook = "YOUR_WEBHOOK_DISCORD_HERE" --Your code of WEBHOOK
+Config.ShowIdCommand = "showid" --Change the command
